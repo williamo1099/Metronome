b7b7f4f07456f28b86cdbbf538f819bd6c4cd50b
William Oktavianus <williamoktavianus1099@gmail.com>
2023-06-15 12:18:49 +0700

    Adding groups and new model
    Add new groups to the project and a new metronome model.

---
+++ Metronome/Model/MetronomeModel.swift
@@ -0,0 +1,14 @@
+//
+//  MetronomeModel.swift
+//  Metronome
+//
+//  Created by William Oktavianus Kurniawan on 15/06/23.
+//
+
+import Foundation
+
+struct Metronome {
+    var bpm: Int
+    var pendulumPosition: Int
+    var isPlaying: Bool
+}
