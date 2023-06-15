4c674a4a664dca2306511b9b54f6620dcc7e06d6
William Oktavianus <williamoktavianus1099@gmail.com>
2023-06-15 12:14:37 +0700

    Code Preparation
    Create a new Xcode Metronome file.

---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by William Oktavianus Kurniawan on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
